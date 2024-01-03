5d57dfd842f04ee0bbda71d24dc11ef90bea13b2
markwst <Wongsathorn_W@gmail.com>
2024-01-03 21:45:37 +0700

    Spring boot init

---
+++ src/main/java/java/mybank_be/MybankBeApplication.java
@@ -0,0 +1,13 @@
+package java.mybank_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/mybank_be/MybankBeApplicationTests.java
@@ -0,0 +1,13 @@
+package java.mybank_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
